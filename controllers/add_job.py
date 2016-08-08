1a7717788ab7eb88f45d17612618656fb8055ba6
PetruninAleksey <petrunin.aleksey.dev@gmail.com>
2016-08-08 10:17:59 +0300

    Modified AddJobController

--- controllers/add_job.py
+++ controllers/add_job.py
@@ -18,8 +18,12 @@ class AddJobController(SchedulerController):
         elif not self._request.method == "POST":
             raise Exception("Unexpected request method type '%s'" % self._request.method)
 
-        job_data = self._verify_job_data()
-        trigger_data = self._verify_trigger_data()
+        required_attrs = ("name", "func", "kwargs", "id", "year", "month", "day", "week", "day_of_week", "hour",
+                          "minute", "second", "start_date", "end_date")
+        data = self._verify_form_data(required_attrs)
+
+        job_data = self._verify_job_data(data)
+        trigger_data = self._verify_trigger_data(data)
 
         job = self._scheduler.add_job(trigger=CronTrigger(timezone=None, **trigger_data), **job_data)
 
@@ -28,16 +32,7 @@ class AddJobController(SchedulerController):
         flash("Job wasn't added")
         return render_template('add.html')
 
-    def _verify_job_data(self):
-        required_attrs = ("name", "func", "kwargs", "id")
-
-        data = self._request.form
-        if not data:
-            raise Exception('Request data is empty')
-        for attr in required_attrs:
-            if attr not in data:
-                raise Exception('Invalid request. Parameter "%s" not found in "%s"' % (attr, data))  # TODO Ex msg
-
+    def _verify_job_data(self, data):
         result = {
             'id': data['id'],
             'func': data['func'],
@@ -46,17 +41,7 @@ class AddJobController(SchedulerController):
         }
         return result
 
-    def _verify_trigger_data(self):
-        required_attrs = ("year", "month", "day", "week", "day_of_week", "hour",
-                          "minute", "second", "start_date", "end_date")
-
-        data = self._request.form
-        if not data:
-            raise Exception('Request data is empty')
-        for attr in required_attrs:
-            if attr not in data:
-                raise Exception('Invalid request. Parameter "%s" not found in "%s"' % (attr, data))  # TODO Ex msg
-
+    def _verify_trigger_data(self, data):
         result = {
             'year': data['year'] or None,
             'month': data['month'] or None,
@@ -70,3 +55,13 @@ class AddJobController(SchedulerController):
             'end_date': data['end_date'] or None,
         }
         return result
+
+    def _verify_form_data(self, required_attrs):
+        data = self._request.form
+
+        if not data:
+            raise Exception('Request data is empty')
+        for attr in required_attrs:
+            if attr not in data:
+                raise Exception('Invalid request data. Parameter "%s" not found in "%s"' % (attr, data))
+        return data
